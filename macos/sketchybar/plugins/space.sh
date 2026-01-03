08eebc49343886e1928b3ffca971c90e627b2cdf
naachiket <naachiket.dev@gmail.com>
2026-01-03 19:40:57 +0530

    Updates

--- macos/sketchybar/plugins/space.sh
+++ macos/sketchybar/plugins/space.sh
@@ -4,4 +4,14 @@
 # the space invoking this script (with name: $NAME) is currently selected:
 # https://felixkratz.github.io/SketchyBar/config/components#space----associate-mission-control-spaces-with-an-item
 
-sketchybar --set "$NAME" background.drawing="$SELECTED"
+# Get the space label from yabai if available
+if command -v yabai &> /dev/null; then
+  SPACE_LABEL=$(yabai -m query --spaces --space "$SID" | jq -r '.label // empty')
+  if [ -n "$SPACE_LABEL" ]; then
+    sketchybar --set "$NAME" label="$SPACE_LABEL" background.drawing="$SELECTED"
+  else
+    sketchybar --set "$NAME" label="" background.drawing="$SELECTED"
+  fi
+else
+  sketchybar --set "$NAME" label="" background.drawing="$SELECTED"
+fi
